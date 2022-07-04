bdbcb6fd8d3daf17b4030fe41ed9ecf5a9d7144e
sora154cm <106710698+sora154cm@users.noreply.github.com>
2022-07-04 11:45:51 +0000

    Finish

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,54 @@
+class BooksController < ApplicationController
+
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def create
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    @book = Book.new(book_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    if @book.save
+    #　フラッシュメッセージの実装
+    flash[:notice] = "Post successfully!!"
+    # 4. トップ画面へリダイレクト
+    redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    flash[:message] = "Edit successfully!!"
+    redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    redirect_to '/books'  # 投稿一覧画面へリダイレクト
+  end
+
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
